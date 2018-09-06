dd0c31d71311e7d3e020f332f02a04906cadf64c
Eric Yan <github@ericyan.me>
2018-09-06 23:48:58 +0800

    Keep big.Int and uint64 as base64url.Value

--- internal/base64url/value.go
+++ internal/base64url/value.go
@@ -3,6 +3,7 @@ package base64url
 import (
 	"encoding/base64"
 	"encoding/json"
+	"math/big"
 )
 
 // A Value holds octets that can be serialized to a base64url-encoded
@@ -20,9 +21,35 @@ func NewValue(data []byte) *Value {
 	return &Value{data}
 }
 
+// NewBigInt creates a new Value representing an big.Int.
+func NewBigInt(x *big.Int) *Value {
+	return NewValue(x.Bytes())
+}
+
+// NewUint64 creates a new Value representing a uint64.
+func NewUint64(x uint64) *Value {
+	// Special case for 0, as big.Int use an empty byte slice (the zero
+	// value to represent 0.
+	if x == 0 {
+		return &Value{[]byte{0}}
+	}
+
+	return NewBigInt(new(big.Int).SetUint64(x))
+}
+
 // Bytes returns the raw, unencoded octets.
 func (val *Value) Bytes() []byte {
 	return val.octets
+}
+
+// BigInt returns the value as a big.Int.
+func (val *Value) BigInt() *big.Int {
+	return new(big.Int).SetBytes(val.octets)
+}
+
+// Uint64 returns the value as a uint64.
+func (val *Value) Uint64() uint64 {
+	return val.BigInt().Uint64()
 }
 
 // MarshalJSON implements the json.Marshaler interface.
--- internal/base64url/value_test.go
+++ internal/base64url/value_test.go
@@ -3,14 +3,11 @@ package base64url
 import (
 	"bytes"
 	"encoding/json"
+	"math/big"
 	"testing"
 )
 
-func TestValue(t *testing.T) {
-	// Test vectors from Appendix C of RFC 7515
-	data := []byte{3, 236, 255, 224, 193}
-
-	val := NewValue(data)
+func testEncoding(t *testing.T, val *Value, data []byte, expected string) {
 	if !bytes.Equal(val.Bytes(), data) {
 		t.Fatalf("invalid value for %v: %v", data, val.Bytes())
 	}
@@ -19,8 +16,8 @@ func TestValue(t *testing.T) {
 	if err != nil {
 		t.Fatal("unexpected error:", err)
 	}
-	if string(encoded) != `"A-z_4ME"` {
-		t.Fatal("expected encoded value:", string(encoded))
+	if string(encoded) != expected {
+		t.Fatal("unexpected encoded value:", string(encoded))
 	}
 
 	decoded := new(Value)
@@ -30,5 +27,44 @@ func TestValue(t *testing.T) {
 	}
 	if !bytes.Equal(decoded.Bytes(), data) {
 		t.Fatalf("decoding error: want %v, got %v", data, decoded.Bytes())
+	}
+}
+
+func TestValue(t *testing.T) {
+	// Test vectors from Appendix C of RFC 7515
+	data := []byte{3, 236, 255, 224, 193}
+	expected := `"A-z_4ME"`
+
+	testEncoding(t, NewValue(data), data, expected)
+}
+
+func TestUint64(t *testing.T) {
+	cases := []struct {
+		x       uint64
+		octets  []byte
+		encoded string
+	}{
+		{0, []byte{0}, `"AA"`},             // RFC 7515, Section 2
+		{65537, []byte{1, 0, 1}, `"AQAB"`}, // RFC 7515, Section 6.3.1.2
+	}
+
+	for _, c := range cases {
+		testEncoding(t, NewUint64(c.x), c.octets, c.encoded)
+	}
+}
+
+func TestZero(t *testing.T) {
+	bigZero := NewBigInt(big.NewInt(0))
+	if len(bigZero.Bytes()) != 0 {
+		t.Error("octets for zero big.Int should be an empty slice")
+	}
+
+	uintZero := NewUint64(0)
+	if len(uintZero.Bytes()) != 1 && uintZero.Bytes()[0] != 0 {
+		t.Error("octets for zero big.Int should be byte{0}")
+	}
+
+	if bigZero.Uint64() != uintZero.Uint64() {
+		t.Error("zero values are not equal")
 	}
 }
